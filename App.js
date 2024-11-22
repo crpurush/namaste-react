712558954b0800d678426fabbace27257de78abc
CORP\crpurush <Reddy.Purushotham.Chennuru.-ND@disney.com>
2024-11-22 12:08:22 +0530

    ep-3 files

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = (<h1 id="heading" tabIndex="2"
-className="Heading">
-    Namaste React using JSX
-</h1>);
-console.log(heading);
+const reactElementSample = <span>sample react element</span>;
+const title = (
+    <h1 className="head" tabIndex="5"> {reactElementSample} Namaste React using JSX 🚀 </h1>
+);
+
+const Heading = () => (
+    <div id="container">
+        {title}
+        <h1 className="heading">Namaste React Functional component</h1>
+    </div>
+);
+
+const reactElementWithComponent = (
+    <div>
+        <Heading /> <span>we are using Functional component in React Element</span>
+    </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render( reactElementWithComponent);
