d47ffdd6ded376cea1c30016c7ba4423fdedb361
CORP\crpurush <Reddy.Purushotham.Chennuru.-ND@disney.com>
2024-11-27 11:46:31 +0530

    hardcoded names

--- App.js
+++ App.js
@@ -1,22 +1,85 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+/**
+ * Main component list in App
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search
+ * - RestrauntContainer
+ * - RestrauntCard
+ *   - Img,
+ *   - Name of Restaurant, Star Ratings, cuisine, delivery time
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ */
 
-const reactElementSample = <span>sample react element</span>;
-const title = (
-    <h1 className="head" tabIndex="5"> {reactElementSample} Namaste React using JSX 🚀 </h1>
-);
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdaMYtVi9_tfNcpsbGGseU6ehYgV9UeU3h7A&s"></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
-const Heading = () => (
-    <div id="container">
-        {title}
-        <h1 className="heading">Namaste React Functional component</h1>
-    </div>
-);
+const RestaurantCard = () => {
+    return (
+        <div className="restaurant-card">
+            <img className="restaurant-logo"
+            alt="restaurant-logo"
+            src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/gp1ityra6utvzqn6ghnv"
+            ></img>
+            <h3 className="restaurant-name">Meghana Foods</h3>
+            <h4>Biryani, North Indian, Asian</h4>
+            <h4>4.4 stars</h4>
+            <h4>38 minutes</h4>
+            
+        </div>
+    )
+}
 
-const reactElementWithComponent = (
-    <div>
-        <Heading /> <span>we are using Functional component in React Element</span>
-    </div>
-);
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="restaurant-container">
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="App">
+            <Header />
+            <Body />
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render( reactElementWithComponent);
+root.render(<AppLayout />);
